b7e3c92ff67b3b4174dbe7cb2dbd7bd3bcabbb30
Russell Snyder <russell@rustybass.com>
2019-05-19 20:59:29 +0200

    session 3 with initial time delay and feddback controls

---
+++ Session3/Source/PluginProcessor.h
@@ -0,0 +1,78 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+#define MAX_DELAY_TIME 2
+//==============================================================================
+/**
+*/
+class DelayAudioProcessor  : public AudioProcessor
+{
+public:
+    //==============================================================================
+    DelayAudioProcessor();
+    ~DelayAudioProcessor();
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (AudioBuffer<float>&, MidiBuffer&) override;
+
+    //==============================================================================
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const String getProgramName (int index) override;
+    void changeProgramName (int index, const String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+private:
+    
+    AudioParameterFloat* mDryWetParameter;
+    AudioParameterFloat* mFeedbackParameter;
+    AudioParameterFloat* mDelayTimeParameter;
+    
+    float mFeedbackLeft;
+    float mFeedbackRight;
+
+    float delayTimeInSamples;
+    float mDelayReadHead;
+    
+    float* mCicularBufferLeft;
+    float* mCicularBufferRight;
+    
+    int mCircularBufferLength;
+    int mCircularBufferWriteHead;
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DelayAudioProcessor)
+};
